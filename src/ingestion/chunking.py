a9a46c436d69383fe0ab3387adde848a0d44a221
Dakshhhhh-ops <24bee040@nith.ac.in>
2026-07-25 02:38:23 +0530

    chunking.py done

--- src/ingestion/chunking.py
+++ src/ingestion/chunking.py
@@ -1,6 +1,27 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from loader import load_documents
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def split_documents():
+    """
+    Splits loaded documents into smaller chunks.
+    """
+
+    documents = load_documents()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    return chunks
+
+
+if __name__ == "__main__":
+    chunks = split_documents()
+
+    print(f"Total Chunks: {len(chunks)}")
+    print(type(chunks[0]))
+    print(chunks[0])
