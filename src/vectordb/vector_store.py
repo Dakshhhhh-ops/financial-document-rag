aafd823fd410b622e739aac78efd24abc8e1f3a6
Dakshhhhh-ops <24bee040@nith.ac.in>
2026-07-25 02:58:50 +0530

    vectore_store.py done

--- src/vectordb/vector_store.py
+++ src/vectordb/vector_store.py
@@ -2,3 +2,15 @@ from langchain_community.vectorstores import FAISS
 
 from ingestion.chunking import split_documents
 from embeddings.embedding import load_embedding_model
+
+def create_vector_store():
+    """
+    Creates and returns the FAISS vector store
+    """
+    chunks=split_documents()
+    embedding_model=load_embedding_model()
+    vector_store=FAISS.from_documents(
+        documents=chunks,
+        embedding=embedding_model
+    )
+    return vector_store
