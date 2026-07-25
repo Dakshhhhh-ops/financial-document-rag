4ca2a27eea46e49c1e4b952ab2209856411677f2
Dakshhhhh-ops <24bee040@nith.ac.in>
2026-07-25 18:48:07 +0530

    llm.py done

--- src/llm/llm.py
+++ src/llm/llm.py
@@ -5,8 +5,18 @@ load_dotenv()
 
 def load_llm():
 
-    llm=ChatGroq(
-        model="llama-3.3-70b-versatile"
-        temperature=0;
+    llm = ChatGroq(
+        model="llama-3.3-70b-versatile",
+        temperature=0
     )
+
     return llm
+
+
+if __name__ == "__main__":
+
+    llm = load_llm()
+
+    response = llm.invoke("Who are you?")
+
+    print(response.content)
